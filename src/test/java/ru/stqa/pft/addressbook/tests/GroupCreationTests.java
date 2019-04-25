c0117a23644c761e7a3424d1e02b18fcb694a308
Pavel <slangtr@yandex.ru>
2019-04-25 21:27:29 +0300

    оставляем дефолтные и не изменяющиеся значение в полях

--- src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,10 +6,10 @@ import ru.stqa.pft.addressbook.model.GroupData;
 public class GroupCreationTests extends TestBase {
 
     @Test
-    public void testGroupCreation() {
+    public void testGroupCreation() throws InterruptedException {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
     }
