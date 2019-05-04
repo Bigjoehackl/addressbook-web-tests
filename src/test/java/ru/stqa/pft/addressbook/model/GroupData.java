a9bb2e4cd7b87a4073f669aac549a107b52fbb8d
Pavel <slangtr@yandex.ru>
2019-05-04 14:15:12 +0300

    добавил сортировку списков

--- src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -11,24 +11,6 @@ public class GroupData {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        if (id != groupData.id) return false;
-        return name != null ? name.equals(groupData.name) : groupData.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + id;
-        return result;
-    }
-
-    @Override
     public String toString() {
         return "GroupData{" +
                 "name='" + name + '\'' +
@@ -36,8 +18,23 @@ public class GroupData {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
--- src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
@@ -38,12 +39,15 @@ public class GroupCreationTests extends TestBase {
         int max = after.stream().max(byId).get().getId();*/
 
 
-        //Способ сравнения с помощью лямбда выражений
+    /*    //Способ сравнения с помощью лямбда выражений
         int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+        group.setId(max);*/
 
-        group.setId(max);
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 
 }
