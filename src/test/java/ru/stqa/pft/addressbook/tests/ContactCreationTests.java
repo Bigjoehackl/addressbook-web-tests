b29cc8815ede03d4d5f8b223ec9630285be9679a
Pavel <slangtr@yandex.ru>
2019-05-10 20:52:16 +0300

    добавил в тест фото с помощью относительного пути

--- src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,13 +3,17 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.io.File;
+
 public class ContactCreationTests extends TestBase {
+
     @Test
     public void testContactCreation() {
         app.goTo().gotoHomePage();
         app.contact().initContactCreation();
+        File photo = new File("src/test/resources/stickeroid_5bf5459dcb8e2.png");
         app.contact().fillContactForm(
-                new ContactData().withFirstname("test_name").withLastname("test_surname").withGroup("test1"), true);
+                new ContactData().withFirstname("test_name").withLastname("test_surname").withPhoto(photo), true);
         app.contact().submitContactCreation();
         app.contact().returnToHomePage();
     }
